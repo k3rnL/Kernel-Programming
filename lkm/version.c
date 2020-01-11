dc123d10da56f9d0eba6e11f5526c270ef1ad47a
Erwan Daniel <erwan.daniel@epitech.eu>
2020-01-11 16:14:23 +0000

    Implement ioctl

--- lkm/version.c
+++ lkm/version.c
@@ -24,10 +24,15 @@ MODULE_AUTHOR("Erwan Daniel <daniel.erwan31@gmail.com>");
 MODULE_DESCRIPTION("Epitech project");
 MODULE_VERSION("1");
 
+#define VERSION_RESET 0
+#define VERSION_MODIFIED 1
+
 struct version_string_holder {
   int  size;
   int  null_byte_pos;
+  int  state;
   char *string;
+  char *original;
 };
 
 struct version_string_holder version_string;
@@ -40,7 +45,7 @@ static ssize_t version_read(struct file * file, char * buf,
   if (*ppos >= version_string.null_byte_pos)
     return 0;
     
-  to_copy = version_string.null_byte_pos - *ppos;
+  to_copy = version_string.size - *ppos;
   if (count < to_copy)
     to_copy = count;
     
@@ -62,21 +67,41 @@ static ssize_t version_write(struct file * file, const char * buf,
 
   to_copy = version_string.size - 1 - *ppos;
   if (count < to_copy)
-    to_copy = count;
+    to_copy = count - 1;
   printk("try to copy %d bytes at %d position, user asked %d bytes\n", to_copy, *ppos, count);
   memcpy(&version_string.string[*ppos], buf, to_copy);
   *ppos += to_copy;
   
   version_string.string[*ppos] = '\0';
   version_string.null_byte_pos = *ppos;
+  version_string.state = VERSION_MODIFIED;
   
   return to_copy;
+}
+
+static long version_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+  if (cmd == VERSION_MODIFIED)
+  {
+    copy_to_user((int32_t*) arg, &version_string.state, sizeof(version_string.state));
+  }
+  else if (cmd == VERSION_RESET)
+  {
+    memcpy(version_string.string, version_string.original, version_string.size - 1);
+    version_string.null_byte_pos = version_string.size - 1;
+    version_string.state = VERSION_RESET;
+  }
+  else
+    return -1;
+  
+  return 0;
 }
 
 static const struct file_operations version_fops = {
  .owner = THIS_MODULE,
  .read = version_read,
- .write = version_write
+ .write = version_write,
+ .unlocked_ioctl = version_ioctl
 };
 
 static struct miscdevice version_dev = {
@@ -85,18 +110,24 @@ static struct miscdevice version_dev = {
  &version_fops
 };
 
+#define VERSION_STRING(state) state ? "VERSION_MODIFIED" : "VERSION_RESET"
+
 static int __init version_init(void)
 {
   int    ret;
 
   version_string.size = strlen(utsname()->version) + 2;
   version_string.string = kmalloc(version_string.size, GFP_KERNEL);
+  version_string.original = kmalloc(version_string.size, GFP_KERNEL);
   
   memcpy(version_string.string, utsname()->version, version_string.size - 1);
-  
   version_string.string[version_string.size - 2] = '\n';
   version_string.string[version_string.size - 1] = '\0';
 
+
+  memcpy(version_string.original, version_string.string, version_string.size - 1);
+  
+  version_string.state = VERSION_RESET;
   version_string.null_byte_pos = version_string.size - 1;  
 
   ret = misc_register(&version_dev);
